4cbfecea66b68ebd57190b6949c94a5d538ea3a9
kepherogalo <kepher.ogalo@student.moringaschool.com>
2023-08-01 19:18:03 +0300

    Defining Routes and Actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,46 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
+
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  # app.rb
+
+  # Define the routes and actions here
+  before do
+    content_type :json
   end
 
-end
+  get "/landlords" do
+    @landlords = Landlord.all
+    @landlords.to_json
+  end
+
+  get "/landlords/:id" do
+    @landlord = Landlord.find(params[:id])
+    @landlord.to_json
+  end
+
+  post "/landlords" do
+    @landlord = Landlord.new(params[:name], params[:email], params[:phone_number])
+    @landlord.to_json
+  end
+
+  put "/landlords/:id" do
+    @landlord = Landlord.find(params[:id])
+    @landlord.update(params[:landlord])
+    end
+  end
+
+  delete "/landlords/:id" do
+    @landlord = Landlord.find(params[:id])
+    @landlord.destroy
+  end
+
+  post "/house_to_rent" do
+    @house = HouseToRent.new(params[:name], params[:address], params[:price_per_month])
+    @house.to_json
+  end
+
+  get "/house_to_rent" do
+    @houses = HouseToRent.all
+    @houses.to_json
+  end
